e80550e91aba6d9063139650ee2befc4b247640e
Luis Carlin <luiscarlin@gmail.com>
2020-01-07 16:10:17 -0500

    add styled components to page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,39 @@
 // you can have multiple page components for different layouts
 
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Header from "./Header";
 import Meta from "./Meta";
 
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  // box shadow
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
 export default props => {
   return (
-    <div>
-      <Meta />
-      <Header />
-      {props.children}
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledPage>
+        <Meta />
+        <Header />
+        <Inner>{props.children}</Inner>
+      </StyledPage>
+    </ThemeProvider>
   );
 };
